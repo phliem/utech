cb962f47f50af2a682b175b2fb95892a71bfaeb0
Liem Pham <liem@busuu.com>
2018-10-30 21:08:10 +0000

    Redesign homescreen

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -1,35 +1,60 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+import ImageSource from '../../asset/design/dev_room.png';
 
 export default class HomeScreen extends Component {
     static navigationOptions = {
-        title: 'Home',
         header: null,
     };
 
-    // static navigationOptions = ({ navigation }) => {
-    //   return {
-    //       title: 'Home',
-    //       headerRight: (
-    //           <Button
-    //               onPress={() => navigation.navigate('MyModal')}
-    //               title="Info"
-    //               color="#000"
-    //           />
-    //       ),
-    //   }
-    // };
-
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>UTech</Text>
-                <Text>Navigate through the techstack</Text>
-                <Button
-                    title="Go to Login"
-                    onPress={() => this.props.navigation.navigate('LoginScreen')}
-                />
+            <View style={styles.container}>
+                <Image style={styles.img} source={ImageSource}></Image>
+                <Text style={styles.title}>UTech</Text>
+                <Text style={styles.subtitle}>Learn about techstack</Text>
+                <TouchableOpacity
+                    style={styles.cta}
+                    onPress={() => this.props.navigation.navigate('TabScreen')}
+                >
+                    <Text style={styles.ctaText}>Let's start</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EBE7E4',
+        alignItems: 'center',
+    },
+    img: {
+        marginTop: 30,
+        marginBottom: 30,
+        width: '100%',
+        height: '60%',
+    },
+    title: {
+        fontSize: 28,
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 18,
+        marginBottom: 15,
+        color: '#797986'
+    },
+    cta: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#40bf80',
+        borderRadius: 20,
+    },
+    ctaText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white',
+    }
+});
